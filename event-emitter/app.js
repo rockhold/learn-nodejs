97e40377bc09952735aefc91445b772522357b0a
Brandon Rockhold <brandon.rockhold@cerner.com>
2015-12-10 12:21:14 -0600

    Updating event-emitter/app.js to consume a config.js file which contains information about the event to emit/listen for.

--- event-emitter/app.js
+++ event-emitter/app.js
@@ -1,13 +1,15 @@
 var Emitter = require('./emitter');
+var eventConfig = require('./config').events;
+
 var emitter = new Emitter();
 
-emitter.on('greet', function() {
+emitter.on(eventConfig.GREET, function() {
     console.log('Somewhere, someone said hello.');
 });
 
-emitter.on('greet', function() {
+emitter.on(eventConfig.GREET, function() {
     console.log('A greeting occurred!');
 });
 
 console.log('Hello!');
-emitter.emit('greet');
+emitter.emit(eventConfig.GREET);
